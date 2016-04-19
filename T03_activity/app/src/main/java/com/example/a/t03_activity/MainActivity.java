ff43059af5afa70c089eb476f1e198b1c2a4a6dd
outinline <ws777@lycos.co.kr>
2016-04-19 10:11:24 +0900

    init

--- T03_activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
+++ T03_activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a.t03_activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
+        String pw = intent.getStringExtra("pw");
         Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onFinishButtonClick(View v){
+
+        Intent intent = new Intent();
+        intent.putExtra("myResult", "abcdef");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 
--- T03_activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
+++ T03_activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
+    final int MY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,18 @@ public class MyActivity extends AppCompatActivity {
     public void onStartButtonClick(View v){
         Intent intent = new Intent(MyActivity.this, MainActivity.class);
         intent.putExtra("id", "abcd");
-        startActivity(intent);
+        intent.putExtra("pw", "efdf");
+        startActivityForResult(intent, MY_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MY_REQUEST){
+            if(resultCode == RESULT_OK){
+                String res = data.getStringExtra("myResult");
+                Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
