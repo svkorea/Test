5024234be782f8f09c833719521957cec9cfeca0
outinline <ws777@lycos.co.kr>
2016-04-19 09:44:19 +0900

    init

--- T02_Menu/app/src/main/java/com/example/a/t02_menu/MainActivity.java
+++ T02_Menu/app/src/main/java/com/example/a/t02_menu/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.a.t02_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        registerForContextMenu(textView);
+    }
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,100,0,"menu1");
+        menu.add(0,101,0,"menu2");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 100){
+            Toast.makeText(this, "Context1", Toast.LENGTH_SHORT).show();
+        }else if(item.getItemId() == 101) {
+            Toast.makeText(this, "Context2", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_test){
+            Toast.makeText(this, "menu 1", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
     }
 }
