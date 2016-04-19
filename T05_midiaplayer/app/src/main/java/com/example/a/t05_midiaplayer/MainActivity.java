9ebcdbf4247ff15d28ee30a2ab2214b99918a911
outinline <ws777@lycos.co.kr>
2016-04-19 17:06:38 +0900

    i

--- T05_midiaplayer/app/src/main/java/com/example/a/t05_midiaplayer/MainActivity.java
+++ T05_midiaplayer/app/src/main/java/com/example/a/t05_midiaplayer/MainActivity.java
@@ -3,20 +3,56 @@ package com.example.a.t05_midiaplayer;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SEEKBAR_VALUE = 1;
     MediaPlayer mp = null;
+    SeekBar seekBar;
+
+    Handler handler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == SEEKBAR_VALUE){
+                seekBar.setProgress(msg.arg1);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser == true){
+                    if(mp != null){
+                        mp.seekTo(progress);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
@@ -24,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         String path = Environment.getExternalStorageDirectory()+ "/Download/abc.mp3";
 
         mp = new MediaPlayer();
+
 
         try {
             mp.setDataSource(path);
@@ -33,8 +70,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mp.start();
+        seekBar.setMax(mp.getDuration());
 
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null){
 
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(mp != null) {
+
+                        Message msg = new Message();
+                        msg.what = SEEKBAR_VALUE;
+                        msg.arg1 = mp.getCurrentPosition();
+
+                        handler.sendMessage(msg);
+                    }
+
+                }
+            }
+        });
+
+        th.start();
 
     }
 
