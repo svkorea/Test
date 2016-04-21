b006b46dd49e5b7290846fbfd3f24b46f2480746
outinline <ws777@lycos.co.kr>
2016-04-21 18:06:21 +0900

    i

---
+++ T18_alterDialog/app/src/main/java/com/example/a/t18_alterdialog/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.a.t18_alterdialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private int DIALOG_1 = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View v){
+
+        showDialog(DIALOG_1);
+
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(id == DIALOG_1){
+             AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("TITLE");
+            builder.setMessage("Message");
+            builder.setCancelable(false);
+            builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            return builder.create();
+        }
+        return null;
+    }
+}
