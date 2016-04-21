ed0cbef5719aefc0f5c1cc11e2227173a96658b8
outinline <ws777@lycos.co.kr>
2016-04-21 18:05:38 +0900

    i

---
+++ T19_fragment/app/src/main/java/com/example/a/t19_fragment/MyFragment.java
@@ -0,0 +1,34 @@
+package com.example.a.t19_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by a on 2016-04-21.
+ */
+public class MyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my, container, false);
+        final TextView textViewCounter = (TextView) view.findViewById(R.id.textViewCounter);
+        Button  btnIncrease = (Button)view.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value = Integer.parseInt(textViewCounter.getText().toString());
+                value++;
+                textViewCounter.setText(""+value);
+            }
+        });
+        return view;
+    }
+
+
+}
